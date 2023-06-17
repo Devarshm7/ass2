6e3a368d6d09bfabbbb18ffe4a38c199cd6691d7
Devarshm7 <136440109+Devarshm7@users.noreply.github.com>
2023-06-17 11:30:06 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,43 @@
+a=5
+b=10
+"""print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("division:",a/b)
+print("Module:",a%b)
+print("Exponent:",a**b)"""
+
+"""c=10
+c+=2
+print("+=",c)
+c-=4
+print("-=",c)
+c*=2
+print("*=",c)
+c/=2
+print("/=",c)
+c%=2
+print("%=",c)"""
+
+"""a=True
+b=False
+print("a and b=",a and b)
+print("a or b=",a or b)
+print("a not =",not a)"""
+
+
+# bitwise operators
+print("bitwise &",a&b)
+print("bitwise |",a|b)
+print("bitwise ^",a^b)
+print("bitwise >>",a>>b)
+print("bitwise <<",a<<b)
+
+# identity operator
+print("is",a is b)
+print("is not",a is not b)
+
+# membership operators
+a="siddhi"
+print('s' in a)
+print("a" not in a)
